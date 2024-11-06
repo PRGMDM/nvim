d284cd0e0258cbaf03e4115cfa02c5319b4cb204
Haoran Dong <haorandm@gmail.com>
2024-11-05 20:31:49 -0600

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+
+-- vim options
+vim.opt.number = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.autoindent = true
+vim.opt.ruler = true
+vim.opt.wildmenu = true
