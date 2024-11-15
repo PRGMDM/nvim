fcd704e3827c96b507e62b2364fdcf88fcdb7dc3
Haoran Dong <haorandm@gmail.com>
2024-11-15 12:34:05 -0800

    indentation

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,29 @@
 return {
-  {
-    -- Smooth scrolling
-    "psliwka/vim-smoothie",
-    init = function()
-      vim.g.smoothie_enabled = 1
-    end,
-  },
-  {
-    -- Colorscheme
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-      style = "night",
-      styles = {
-        sidebars = "dark",
-	floats = "dark",
-	comments = { italic = false },
-	keywords = { italic = false },
-      },
+    {
+        -- Smooth scrolling
+        "psliwka/vim-smoothie",
+        init = function()
+            vim.g.smoothie_enabled = 1
+        end
     },
-  },
-  {
-    -- fuzzy finder
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+    {
+        -- Colorscheme
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = {
+            style = "night",
+            styles = {
+                sidebars = "dark",
+	            floats = "dark",
+	            comments = { italic = false },
+	            keywords = { italic = false }
+            }
+        }
+    },
+    {
+        -- fuzzy finder
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    }
 }
